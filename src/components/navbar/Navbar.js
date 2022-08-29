64e284b1757f5a94f678e248c10dd027490955eb
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-08-30 04:50:12 +0600

    created toggle navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                         <div className='gpt3__navbar-menu_container scale-up-center'>
                             <div className="gpt3__navbar-menu_container_links">
                                 <Menu/>
-                                <div className="gpt3__navbar-sign">
+                                <div className="gpt3__navbar-menu_container-links-sign">
                                     <p>Sign in</p>
                                     <button type='button'>Sign Up</button>
                                 </div>  
