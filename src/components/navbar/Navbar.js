4680029d9a0ea67d9d1e5539d5aa93a47102e2fb
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-08-30 04:39:57 +0600

    added sign buttons for toggle menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,14 +17,7 @@ const Menu = () => (
 
 // Created Reusable Menu Items Component
 
-// Created Reusable Sign Menu Items
-const MenuSign = () => (
-    <>
-        <p>Sign in</p>
-        <button type='button'>Sign Up</button>
-    </>
-)
-// Created reusable Sign menu Items
+
     
 
 
@@ -42,30 +35,33 @@ const Navbar = () => {
                 <div className="gpt3__navbar-links_container">
                     <Menu/>
                 </div>
-            </div> 
-            
-                <div className="gpt3__navbar-sign">
-                    <MenuSign/>                  
-                </div>
-                <div className="gpt3__navbar-menu">
-                    {
-                        toggleMenu
-                        ? <RiCloseLine color='#fff' size={27} onClick ={()=>setToggleMenu(false)}/>
-                        : <RiMenu3Line color='#fff' size={27} onClick ={()=>setToggleMenu(true)}/>
-                    }
-                    {
-                        toggleMenu && (
-                            <div className='gpt3__navbar-menu_container scale-up-center'>
-                                <div className="gpt3__navbar-menu_container_links">
-                                    <Menu/>
-                                    <MenuSign/>
-                                </div>
-
+            </div>  
+            <div className="gpt3__navbar-sign">
+                <p>Sign in</p>
+                <button type='button'>Sign Up</button>
+            </div>                  
+            <div className="gpt3__navbar-menu">
+                {
+                    toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick ={()=>setToggleMenu(false)}/>
+                    : <RiMenu3Line color='#fff' size={27} onClick ={()=>setToggleMenu(true)}/>
+                }
+                {
+                    toggleMenu && (
+                        <div className='gpt3__navbar-menu_container scale-up-center'>
+                            <div className="gpt3__navbar-menu_container_links">
+                                <Menu/>
+                                <div className="gpt3__navbar-sign">
+                                    <p>Sign in</p>
+                                    <button type='button'>Sign Up</button>
+                                </div>  
                             </div>
-                        )
-                    }
 
-                </div>
+                        </div>
+                    )
+                }
+
+            </div>
                 
         </div>
     );
