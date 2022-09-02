cad05532ba2499aa833ea3410a87a6793a976260
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-09-03 00:11:01 +0600

    working with brand section, compressed and converted images to webp

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from "./../../assets/google.webp"
+import slack from "./../../assets/slack.webp"
+import atlassian from "./../../assets/atlassian.webp"
+import dropbox from "./../../assets/dropbox.webp"
+import shopify from "./../../assets/shopify.webp"
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./navbar.css"
-import logo from "./../../assets/logo.svg"
+import logo from "./../../assets/main_logo.webp"
 import {RiCloseLine, RiMenu3Line} from "react-icons/ri"
 
 
